0a5a6e680eebb9a6cdd6346b23975c3523afe083
dgeene <david.geene@gmail.com>
2018-01-25 13:18:26 -0500

    add basix input

--- misc/stdlib/vector.cpp
+++ misc/stdlib/vector.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
+#include <string>
 #include <vector>
+
+//exclude using namespace std for now
 
 int main()
 {
-    std::vector<int> inputs;
+    std::vector<int> letters;
+    std::string input = "";
+
+    std::cout << "--Storing letters in a vector--\n";
+    std::cout << "Enter a word to store\n";
+
+    getline(std::cin, input);
+    std::cout << "You entered: " << input << std::endl;
+
 }
