34678951a23e89cc5117faf22d1eea834ead8132
dgeene <david.geene@gmail.com>
2018-02-05 09:36:43 -0500

    add assignment start files

---
+++ class/Assignment_1/Date.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include "Date.h"
+
+using namespace std;
+
+    //int month, day, year;
+
+Date::Date() {
+}
+
+void Date::print() {
+        cout << "Hello from date class";
+}
---
+++ class/Assignment_1/Date.h
@@ -0,0 +1,8 @@
+class Date
+{
+    private:
+        int month, day, year;
+    public:
+        Date();
+        void print();
+};
