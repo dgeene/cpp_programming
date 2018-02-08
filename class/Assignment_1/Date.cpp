62e58236bef579af3e8675220a04abd0d5ca7688
dgeene <david.geene@gmail.com>
2018-02-08 13:12:21 -0500

    add getMonthName

--- class/Assignment_1/Date.cpp
+++ class/Assignment_1/Date.cpp
@@ -29,6 +29,54 @@ Date::Date(int month, int day, int year) {
     mDay = day;
 }
 
+const char *Date::getMonthName() {
+    const char *name;
+
+    switch(mMonth) {
+        case 1:
+            name = "January";
+            break;
+        case 2:
+            name = "February";
+            break;
+        case 3:
+            name = "March";
+            break;
+        case 4:
+            name = "April";
+            break;
+        case 5:
+            name = "May";
+            break;
+        case 6:
+            name = "June";
+            break;
+        case 7:
+            name = "July";
+            break;
+        case 8:
+            name = "August";
+            break;
+        case 9:
+            name = "September";
+            break;
+        case 10:
+            name = "October";
+            break;
+        case 11:
+            name = "November";
+            break;
+        case 12:
+            name = "December";
+            break;
+        default:
+            name = "Unknown Month!";
+            break;
+    }
+
+    return name;
+}
+
 /*
  * Prints the date to stdout in the form mm/dd/yyyy
  */
@@ -37,4 +85,11 @@ void Date::print() {
             << setfill('0') << setw(2) << mMonth << "/"
             << setfill('0') << setw(2) << mDay << "/"
             << mYear <<  endl;
+}
+
+/*
+ * Prints the date in long format February 2, 2018
+ */
+void Date::printLong() {
+    cout << "printlong" << endl;
 }
--- class/Assignment_1/Date.h
+++ class/Assignment_1/Date.h
@@ -5,7 +5,9 @@ class Date
     public:
         Date();
         Date(int month, int day, int year);
+        const char *getMonthName();
         void print();
+        void printLong();
         int getDay() { return mDay; }
         int getMonth() { return mMonth; }
         int getYear() { return mYear; }
