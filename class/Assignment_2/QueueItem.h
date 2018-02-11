97d129b0d9dce92be27fc3f2146dc704c90fbefd
dgeene <david.geene@gmail.com>
2018-02-11 15:53:19 -0500

    add assignment 2 files

---
+++ class/Assignment_2/QueueItem.h
@@ -0,0 +1,12 @@
+class QueueItem {
+    public:
+        QueueItem(const char *pData, int id);
+        void setNext(QueueItem *pItem);
+        QueueItem* getNext() const;
+        int getId() const;
+        const char* getData() const;
+    private:
+        char _data[30]; // data value (null temrinated character string)
+        const int _itemId;
+        QueueItem* _pNext;
+};
