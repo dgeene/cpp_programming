8202e43ba873086a97751744cc75122621083254
dgeene <david.geene@gmail.com>
2018-02-27 09:19:13 -0500

    add RINT files

---
+++ class/Assignment_4/RINT.h
@@ -0,0 +1,7 @@
+#include <iostream>
+
+class RINT {
+    public:
+    private:
+        int mNum;
+};
---
+++ class/Assignment_4/rint_main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "RINT.h"
+
+using namespace std;
+
+int main() {
+    /*RINT x, y = 4;
+    int a = 5, b = 2;
+
+    cout << x << endl;
+    cin >> x;
+
+    x = x + 1;
+    y = x - a;*/
+
+    //x++;       // should generate error
+    //y += 3;    // should generate error
+}
