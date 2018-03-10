646ad29164251d8e5cc770706084962ae88ac2f7
dgeene <david.geene@gmail.com>
2018-03-10 18:23:23 -0500

    add shape entry and print loops

--- class/Assignment_5/main.cpp
+++ class/Assignment_5/main.cpp
@@ -4,14 +4,36 @@
 
 using namespace std;
 
+#define SIZE 10
+
 int main() {
 
-    Shape* myShape;
+    Shape* myShapes[SIZE] = { 0 };
 
     cout << "Enter a list of shapes OR - 'done' to end" << endl;
 
-    while (getShape() != NULL) {
+    // prompt the user for shape input
+    int count = 0;
+    Shape* shapePtr;
+    while (true) {
+        shapePtr = getShape();
+        if (shapePtr == NULL || count > SIZE)
+            break;
 
+        myShapes[count] = shapePtr;
+        ++count;
+    }
+
+    // print the list of shapes
+    if (myShapes[0] != NULL) {
+        cout << endl << endl
+        << "---------------------------------" << endl;
+        cout << "The list of shapes entered" << endl;
+        for (int i = 0; i < SIZE; i++) {
+            if (myShapes[i] == NULL)
+                break;
+            cout << myShapes[i]->toString() << endl;
+        }
     }
 
     cout << "end" << endl;
