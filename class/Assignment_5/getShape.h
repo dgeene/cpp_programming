4986fb0cfcb21d3d9c60b772923f46ad9c493cbe
dgeene <david.geene@gmail.com>
2018-03-10 17:34:40 -0500

    finish getshape

--- class/Assignment_5/getShape.h
+++ class/Assignment_5/getShape.h
@@ -1,10 +1,18 @@
 #include <iostream>
 #include "Shape.h"
+#include "Circle.h"
+#include "Square.h"
+#include "Rectangle.h"
 
 using std::cout;
 using std::endl;
 using std::cin;
 
+/*
+ * Prompt the user to enter a shape color and type.
+ * Additional prompts will be asked for certain shape properties
+ * Returns NULL is 'done' is entered or an invalid shape type is entered
+ */
 Shape* getShape() {
     string color; // shape color
     string type;  // shape type
@@ -16,16 +24,19 @@ Shape* getShape() {
     if (color == "done") {
         return myShape;
     }
+
     cout << "Enter the shape type.. Options are [ circle | square | rectangle ]" << endl;
     cin >> type;
     if (type == "circle") {
         double radius;
         cout << "Enter the radius.." << endl;
         cin >> radius;
+        myShape = new Circle(color, radius);
     } else if (type == "square") {
         double side;
         cout << "Enter the length of a side.." << endl;
         cin >> side;
+        myShape = new Square(color, side);
     } else if (type == "rectangle") {
         double length;
         double width;
@@ -33,6 +44,7 @@ Shape* getShape() {
         cin >> width;
         cout << "Enter the length.." << endl;
         cin >> length;
+        myShape = new Rectangle(color, width, length);
     }
     return myShape;
 }
